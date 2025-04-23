9be04bbf645eafe722e655c7d084219e40ef6ff1
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-04-23 23:25:56 +0800

    Refactor server.js to improve code organization and clarity by grouping app initialization and route setup

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import promptRoutes from './routes/promptRoutes.js';
 
-dotenv.config();
-const app = express();
-app.use(cors());
-app.use(express.json());
-connectDB()
+// App initialization
+dotenv.config(); // Load environment variables from .env file
+const app = express(); // Create an Express application
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Parse incoming JSON requests
 
+// Routes
+app.use('/api/prompts', promptRoutes);
+
+connectDB() // Connect to MongoDB database
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
